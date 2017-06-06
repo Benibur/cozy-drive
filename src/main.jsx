3d6ca49fb34b1f462aa3e4b88d5b5f1e676c6507
Ben <mail1@sonadresse.com>
2017-06-06 10:01:59 +0200

    autocomplete from algolia inserted, fuzzy search ok, boldify ok BUT no CSS

--- src/main.jsx
+++ src/main.jsx
@@ -18,6 +18,13 @@ import eventTrackerMiddleware from './middlewares/EventTracker'
 import filesApp from './reducers'
 import AppRoute from './components/AppRoute'
 
+// ------------------------------------------------------------------
+//-- BJA : for the hacked search-bar
+import autocompleteAlgolia from 'autocomplete.js'
+import fuzzaldrinPlus from 'fuzzaldrin-plus'
+//-- \BJA
+// ------------------------------------------------------------------
+
 const loggerMiddleware = createLogger()
 
 if (__DEVELOPMENT__) {
@@ -72,4 +79,121 @@ document.addEventListener('DOMContentLoaded', () => {
       </Provider>
     </I18n>
   ), root)
+
+
+
+  // ------------------------------------------------------------------
+  //-- BJA : fort the hacked search-bar
+  // insert a hacked search field in the cozy bar
+  var searchInput   = document.createElement('input')
+  searchInput.setAttribute('id',`search-bar`)
+  searchInput.setAttribute('style',`background-color:yellow;
+    border:1px solid black;
+    height:20px;
+    width:100px;
+    cursor:pointer;
+    transition: top .2s ease;`)
+  var target = document.querySelector('.coz-nav')
+  target.parentElement.insertBefore(searchInput,target)
+
+  autocompleteAlgolia('#search-bar', { hint: true }, [
+    {
+      source: function (query, cb) {
+        cb(fuzzaldrinPlusSearch(query))
+      } ,
+      displayKey: 'path',
+      templates: {
+        suggestion: function(suggestion) {
+          return suggestion.html
+        }
+      }
+    }
+  ])
+  // ------------------------------------------------------------------
+  // 2] prepare the Search options for fuzzaldrin
+
+  const fuzzaldrinPlusSearch = function (query) {
+    const results  = fuzzaldrinPlus.filter(list, query, {key: 'path'})
+    for (let res of results){
+      res.html = basiqueBolderify(query, res.path)
+    }
+    return results
+  }
+
+  const basiqueBolderify = function (query, path) {
+    var words = query.split(' ')
+    words = words.filter(function (item) { return (item !== '') })
+    var startIndex = 0
+    var nextWordOccurence, html = ''
+    const lastIndex = path.length
+    while (startIndex<lastIndex) {
+      nextWordOccurence = nextWord(path, words, startIndex)
+      if (!nextWordOccurence) {
+        break
+      }
+      html += `${path.slice(startIndex, nextWordOccurence.start)}<b>${nextWordOccurence.word}</b>`
+      startIndex = nextWordOccurence.end
+    }
+    html += path.slice(startIndex)
+    return html
+  }
+
+  const nextWord = function (path, words, startIndex) {
+    path = path.toLowerCase()
+    var I = path.length
+    var W=''
+    var i = -1
+    for (let w of words) {
+      i = path.indexOf(w.toLowerCase(),startIndex)
+      if (i<I && -1<i) {
+        I = i
+        W = w
+      }
+    }
+    if (i == -1) {
+      return undefined
+    }else {
+      return {word:W, start:I, end:I+W.length}
+    }
+  }
+  const list = [
+    {path:"/Administratif"},
+    {path:"/Administratif/Bank statements"},
+    {path:"/Administratif/Bank statements/Bank Of America"},
+    {path:"/Administratif/Bank statements/Deutsche Bank"},
+    {path:"/Administratif/Bank statements/Société Générale"},
+    {path:"/Administratif/CPAM"},
+    {path:"/Administratif/EDF"},
+    {path:"/Administratif/EDF/Contrat"},
+    {path:"/Administratif/EDF/Factures"},
+    {path:"/Administratif/Emploi"},
+    {path:"/Administratif/Impôts"},
+    {path:"/Administratif/Logement"},
+    {path:"/Administratif/Logement/Loyer 158 rue de Verdun"},
+    {path:"/Administratif/Orange"},
+    {path:"/Administratif/Pièces identité"},
+    {path:"/Administratif/Pièces identité/Carte identité"},
+    {path:"/Administratif/Pièces identité/Passeport"},
+    {path:"/Administratif/Pièces identité/Permis de conduire"},
+    {path:"/Appareils photo"},
+    {path:"/Boulot"},
+    {path:"/Cours ISEN"},
+    {path:"/Cours ISEN/CIR"},
+    {path:"/Cours ISEN/CIR/LINUX"},
+    {path:"/Cours ISEN/CIR/MICROCONTROLEUR"},
+    {path:"/Cours ISEN/CIR/RESEAUX"},
+    {path:"/Cours ISEN/CIR/TRAITEMENT_SIGNAL"},
+    {path:"/Divers photo"},
+    {path:"/Divers photo/wallpapers"},
+    {path:"/Films"},
+    {path:"/Notes"},
+    {path:"/Notes/Communication"},
+    {path:"/Notes/Notes techniques"},
+    {path:"/Notes/Recrutement"},
+    {path:"/Projet appartement à Lyon"},
+    {path:"/Vacances Périgord"}
+  ]
+
+  //-- \BJA
+  // ------------------------------------------------------------------
 })
